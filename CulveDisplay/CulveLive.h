1e5f94699398782ad8a1861e96a21d2bd3b1c6cf
王源果 <g4idrijs@gmail.com>
2014-12-15 19:45:35 +0800

    创建工程
    添加文件

---
+++ CulveDisplay/CulveLive.h
@@ -0,0 +1,32 @@
+//
+//  CulveLive.h
+//  CulveDisplay
+//
+//  Created by 王源果 on 14/12/15.
+//  Copyright (c) 2014年 g4idrijs. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface PointContainer : NSObject
+
+@property (nonatomic , readonly) NSInteger numberOfRefreshElements;
+@property (nonatomic , readonly) NSInteger numberOfTranslationElements;
+@property (nonatomic , readonly) CGPoint *refreshPointContainer;
+@property (nonatomic , readonly) CGPoint *translationPointContainer;
+
++ (PointContainer *)sharedContainer;
+
+//刷新变换
+- (void)addPointAsRefreshChangeform:(CGPoint)point;
+//平移变换
+- (void)addPointAsTranslationChangeform:(CGPoint)point;
+
+@end
+
+@interface CulveLive : UIView
+
+- (void)fireDrawingWithPoints:(CGPoint *)points pointsCount:(NSInteger)count;
+
+@end
---
+++ CulveDisplay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CulveDisplay
+//
+//  Created by 王源果 on 14/12/15.
+//  Copyright (c) 2014年 g4idrijs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
